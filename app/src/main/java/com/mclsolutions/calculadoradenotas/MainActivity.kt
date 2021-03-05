cdf9df70c1158b9dffbf241a69e1e1fe3db9b4e2
MarceloCLopes <marc01mcl@gmail.com>
2021-03-05 12:37:20 -0300

    implemented codes in the activity main

--- app/src/main/java/com/mclsolutions/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/mclsolutions/calculadoradenotas/MainActivity.kt
@@ -2,10 +2,28 @@ package com.mclsolutions.calculadoradenotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calcular = btnCalcular
+        val resultado = textResultado
+
+        calcular.setOnClickListener {
+
+            val nota1 = Integer.parseInt(editNota1.text.toString())
+            val nota2 = Integer.parseInt(editNota2.text.toString())
+            val media = (nota1 + nota2) / 2
+            val faltas = Integer.parseInt(editNumeroFaltas.text.toString())
+
+            if (media >= 6 && faltas <= 10){
+                resultado.text = "Aluno Aprovado\nNota final: $media\nFaltas: $faltas"
+            } else{
+                resultado.text ="Aluno Reprovado\nNota final: $media\nFaltas: $faltas"
+            }
+        }
     }
 }
